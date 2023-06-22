48346a7c07b5c75006b3b185b96a58a4cd62151d
Michael Jecmen <mjjecmen@gmail.com>
2023-06-21 17:03:37 -0700

    add notes from raspi messaround

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { KindleClippingsExtractor } from "./kindle/clippings";
 // Create an instance of the KindleFileCopier class
 const copier = new KindleClippingsExtractor(
 	'/media/pi/Kindle',
-	'/media/pi/Kindle/documents/My Clippings.txt',
+	'/media/pi/Kindle/documents/My Clippings.txt', // <-- these filepaths are actually correct
 	`${process.env.HOME}/Desktop`
 );
 
